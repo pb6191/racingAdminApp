8ccb941e701cf3e19d4bc15a02f45a0778407190
Puneet Bhargava <pbhargava936@gmail.com>
2021-12-29 22:29:28 -0800

    racing admin app

---
+++ OptionButton3.gd
@@ -0,0 +1,27 @@
+extends OptionButton
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$".".add_item("0")
+	$".".add_item("1")
+	$".".add_item("2")
+	$".".add_item("3")
+	$".".add_item("4")
+	$".".add_item("5")
+	$".".add_item("6")
+	$".".add_item("7")
+	$".".add_item("8")
+	$".".add_item("9")
+	$".".add_item("10")
+	$".".add_item("11")
+	$".".add_item("12")
+
+	# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ global.gd
@@ -0,0 +1,47 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var maxSpeedIP
+var accelerationIP
+var nBackIP
+var strtFuelIP
+var bonusFuelIP
+var nBackIntervalIP
+var tLimit
+var taskIP
+var setIntervalIP
+var setNIP
+var setNExtraIP
+var penaltyIP
+var nFlankIP
+var respLimitIP
+var centreShift
+var sizeIP
+var url
+var endpt
+
+var poplnID
+var partcnID
+var sessID
+var measrID
+
+var measrTypeID
+var startScrn
+
+var dict = {"thisSession": [{"speed": "", "acc": "", "nback": "", "respLimit": "", "startFuel": "", "bonusFuel": "", "stimInterval": "", "timeLimit": "", "task": "", "stimDuration": "", "intraN": "", "extraN": "", "penalty": "", "flankerN": "", "centreUp": "", "symComplexity": "", "trials": [{"timeStamp": "", "totalTimeElapsed": "", "trialTimeElapsed": "", "event":"", "eventDesc": {"target":"", "leftMost":{"color": "", "shapeDesc": "", "size": ""}, "left":{"color": "", "shapeDesc": "", "size": ""}, "right":{"color": "", "shapeDesc": "", "size": ""}, "rightMost":{"color": "", "shapeDesc": "", "size": ""}, "centre":{"color": "", "shapeDesc": "", "size": "", "upShift": ""}}, "distFrmCentre": "", "distanceCovered": "", "fuelStatus": "", "timeDriven": "", "currentTrial": ""}]}]}
+var dictSet = {"speed": "", "acc": "", "nback": "", "respLimit": "", "startFuel": "", "bonusFuel": "", "stimInterval": "", "timeLimit": "", "task": "", "stimDuration": "", "intraN": "", "extraN": "", "penalty": "", "flankerN": "", "centreUp": "", "symComplexity": "", "startScreen": ""}
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
